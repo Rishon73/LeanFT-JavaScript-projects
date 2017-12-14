941b8982f016d036cec0eb097b803a2858ab6fc4
Rishon73 <sshiff@gmail.com>
2017-12-14 18:51:03 -0500

    adding some logs to this script

--- set-lft-conf-env-var.sh
+++ set-lft-conf-env-var.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-launchctl setenv LFT_TEST_CONFIG /Applications/LeanFT/JSconfig.json
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters."
+    echo "Expecting a full path to a json config file"
+    exit
+fi
 
-#launchctl getenv LFT_TEST_CONFIG
+launchctl setenv LFT_TEST_CONFIG $1
+
+echo "LFT_TEST_CONFIG environment variable is set to: " $(launchctl getenv LFT_TEST_CONFIG)
+
 #launchctl unsetenv LFT_TEST_CONFIG
