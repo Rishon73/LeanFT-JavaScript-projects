3c12528e8fd76fafd2db63513e7e03a15c8db207
Rishon73 <sshiff@gmail.com>
2017-12-14 20:54:19 -0500

    OK. This will only work on Mac (for now)...

--- set-lft-conf-env-var-Mac.sh
+++ set-lft-conf-env-var-Mac.sh
@@ -9,11 +9,11 @@ fi
 
 if [[ $(uname -s) == Linux ]]
 then
-    export LFT_TEST_CONFIG=$1
-    echo $(env)
+    echo "This script only works on Mac."
+    echo "To set evn var on Linux, use \"export LFT_TEST_CONFIG=<FULL_PATH_TO_CONF_FILE>\""
+    exit
 else
     launchctl setenv LFT_TEST_CONFIG $1
     echo "LFT_TEST_CONFIG environment variable is set to: " $(launchctl getenv LFT_TEST_CONFIG)
     #launchctl unsetenv LFT_TEST_CONFIG
-
 fi
