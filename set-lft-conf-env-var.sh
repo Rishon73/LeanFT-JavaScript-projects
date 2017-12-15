6db2c1324d2ccd65f423dbf8834b82e9c7748877
Rishon73 <sshiff@gmail.com>
2017-12-14 19:59:21 -0500

    Handling linux and Mac

--- set-lft-conf-env-var.sh
+++ set-lft-conf-env-var.sh
@@ -6,8 +6,14 @@ if [ "$#" -ne 1 ]; then
     exit
 fi
 
-launchctl setenv LFT_TEST_CONFIG $1
 
-echo "LFT_TEST_CONFIG environment variable is set to: " $(launchctl getenv LFT_TEST_CONFIG)
+if [[ $(uname -s) == Linux ]]
+then
+    export LFT_TEST_CONFIG=$1
+    echo $(env)
+else
+    launchctl setenv LFT_TEST_CONFIG $1
+    echo "LFT_TEST_CONFIG environment variable is set to: " $(launchctl getenv LFT_TEST_CONFIG)
+    #launchctl unsetenv LFT_TEST_CONFIG
 
-#launchctl unsetenv LFT_TEST_CONFIG
+fi
